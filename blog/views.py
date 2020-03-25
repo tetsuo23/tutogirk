0b789942d7b883cf696066cc5767b5db41b7c2df
annick17 <annick2525@orange.fr>
2020-03-25 18:50:09 +0100

    je ne sais pas

--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def base(request):
+    return render(request, 'blog/base.html', {})
