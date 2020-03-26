8444ae95f64538c5ac10405dc0c7adf814645543
tetsuo23 <carretmo@gmail.com>
2020-03-26 19:52:05 +0100

    mess

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     #chemin vers le formulaire de post de nouveau contenu
-
+     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+#chemin vers le formulaire d'édition de contenu
     
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -41,4 +41,19 @@ def post_new(request):
             # renvoi le nouveau post à la page post_detail avec une valeur de clé
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blog/post_new.html', {'form': form})
+    #renvoie au formulaire de création du blog concerné pour faire des modifs
